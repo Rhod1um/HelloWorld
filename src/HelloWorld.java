3a877d5f276bb7deadf5c318bc6911deace8b570
Veronica <frida-nanni@hotmail.com>
2022-02-17 22:00:02 +0100

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        new HelloWorld().printHelloWorld();
+    }
+
+    void printHelloWorld (){
+        System.out.println("Hello World");
+    }
+
+}
